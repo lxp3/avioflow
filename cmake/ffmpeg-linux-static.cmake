e2aec4e6afaaac128fe26407cf821ec7a063cb32
lxp3 <121425986+lxp3@users.noreply.github.com>
2026-01-26 16:30:17 +0800

    fix node compile error in linux

--- cmake/ffmpeg-linux-static.cmake
+++ cmake/ffmpeg-linux-static.cmake
@@ -5,13 +5,17 @@ set(LIB_TYPE STATIC)
 # When linking FFmpeg static libraries on Linux, we need to link several 
 # system libraries that FFmpeg depends on (e.g. math library, threading, dynamic loader).
 
+# apt install -y zlib1g-dev libbz2-dev liblzma-dev libdrm-dev libva-dev
 set(FFMPEG_SYSTEM_LIBS
-    z
-    bz2
-    lzma
+    z       # apt install zlib1g-dev
+    bz2     # apt install libbz2-dev
+    lzma    # apt install liblzma-dev
     m
     dl
     pthread
+    rt
+    drm     # apt install libdrm-dev
+    va      # apt install libva-dev
 )
 
 # Apply these to all FFmpeg targets
